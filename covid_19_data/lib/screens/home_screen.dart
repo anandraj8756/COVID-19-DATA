1cd4766d2d8ffb0155661a2aba1813efd0f8a512
Anand Raj <anandallen95@gmail.com>
2020-06-01 05:16:16 +0530

    added call buttom

--- covid_19_data/lib/screens/home_screen.dart
+++ covid_19_data/lib/screens/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_country_picker/flutter_country_picker.dart';
+
 
 class HomeScreen extends StatefulWidget{
 
@@ -8,6 +10,7 @@ class HomeScreen extends StatefulWidget{
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  Country _selectedCountry;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,18 +39,83 @@ class _HomeScreenState extends State<HomeScreen> {
           bottomRight: Radius.circular(40.0)
         )
         ),
-        child: Column(children: <Widget>[
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
           Padding(
             padding: const EdgeInsets.only(top: 20.0, bottom: 25.0),
             child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
                 Text("Covid-19",
                 style: TextStyle(color: Colors.white,
-                fontSize: 28.0),)
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold
+                ),
+                ),
+                Container(
+                  height: 40.0,
+                  padding: EdgeInsets.symmetric(horizontal: 10.0),
+                  decoration: BoxDecoration(color: Colors.white,
+                  borderRadius: BorderRadius.circular(20.0)),
+                  child: CountryPicker(onChanged: (Country country) {
+
+                    setState(() {
+                      _selectedCountry = country;
+                    });
+
+
+                  },
+                  selectedCountry: _selectedCountry,
+                  ),
+                )
               ],
             ),
+          ),
+          Text("Are you felling sick?",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+            fontSize: 20.0
+          ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 10.0),
+            child: Text("If you feel sick with any of covid-19 symptoms please call or sms us now",
+          style: TextStyle(fontSize: 15.0, color: Colors.white, height: 1.5)
           )
-        ],),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 20.0),
+            child: Row(
+            children: <Widget>[
+              FlatButton.icon(
+                padding: 
+                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                color: Color(0XFFF4D58),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30.0)),
+                label: Text("Call Now",
+                style: TextStyle(color: Colors.white)),
+
+                onPressed: () {}, icon: Icon(Icons.phone, color: Colors.white,
+                )),
+                FlatButton.icon(
+                padding: 
+                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                color: Color(0XFF4D79FF),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30.0)),
+                label: Text("Send SMS",
+                style: TextStyle(color: Colors.white)),
+
+                onPressed: () {}, icon: Icon(Icons.phone, color: Colors.white,)
+                )
+            ],
+          ),
+        ),
+        ],
+        ),
 
       );
 
