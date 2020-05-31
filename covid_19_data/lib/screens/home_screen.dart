ada6e36bb5a625e42734b9f3454a9d5428dca8e0
Anand Raj <anandallen95@gmail.com>
2020-06-01 00:35:34 +0530

    second commit

--- covid_19_data/lib/screens/home_screen.dart
+++ covid_19_data/lib/screens/home_screen.dart
@@ -12,10 +12,43 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(leading: IconButton(icon: Icon(Icons.sort), onPressed: () {}),
+      appBar: AppBar(
+        elevation: 0,
+        leading: IconButton(icon: Icon(Icons.sort), onPressed: () {}),
       actions: <Widget>[
         IconButton(icon: Icon(Icons.notifications_none), onPressed: () {})
       ],
-    ));
-  }
+    ),
+    body: ListView(
+      children: <Widget>[_header()],
+    ),
+    );
+      
+    }
+
+    Widget _header() {
+      return Container(
+        height: 250.0,
+        padding: EdgeInsets.symmetric(horizontal: 20.0),
+        decoration: BoxDecoration(color: Theme.of(context).primaryColor,
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(40.0),
+          bottomRight: Radius.circular(40.0)
+        )
+        ),
+        child: Column(children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(top: 20.0, bottom: 25.0),
+            child: Row(
+              children: <Widget>[
+                Text("Covid-19")
+              ],
+            ),
+          )
+        ],),
+
+      );
+
+
+    }
 }
